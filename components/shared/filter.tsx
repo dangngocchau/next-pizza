c3a4e2d7299ae50db69beb71e298547948457098
Chau Dang <dnchau2310@gmail.com>
2024-10-15 01:53:10 +0700

    Implement slider

--- components/shared/filter.tsx
+++ components/shared/filter.tsx
@@ -1,4 +1,5 @@
-import { FilterCheckbox, Title } from "@/components/shared";
+import { FilterCheckbox, RangeSlider, Title } from "@/components/shared";
+import { Input } from "@/components/ui";
 import React from "react";
 
 type Props = {
@@ -12,6 +13,26 @@ export const Filters = ({ className }: Props) => {
             <div className="flex flex-col gap-4">
                 <FilterCheckbox text="Checkbox 1" value="1" />
                 <FilterCheckbox text="Checkbox 2" value="2" />
+            </div>
+
+            <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+                <p className="font-bold mb-3">Price:</p>
+                <div className="flex gap-3 mb-5">
+                    <Input
+                        type="number"
+                        placeholder="0"
+                        min={0}
+                        max={1000}
+                        defaultValue={0}
+                    />
+                    <Input
+                        type="number"
+                        min={100}
+                        max={1000}
+                        defaultValue={1000}
+                    />
+                </div>
+                <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
             </div>
         </div>
     );
